4f740b54110c2ebf9becc2e8ea3f5866c2773b9b
Kuzyn Procesor <45760981+ProcesorDEV@users.noreply.github.com>
2019-10-20 12:35:59 +0200

    Wersja 2.0
    W tej wersji:
    - dodanie Dynmapy na stronie głównej
    - dokończenie poprawy główek (z pierwszej wersji)

--- admin/tab/index.php
+++ admin/tab/index.php
@@ -1,5 +1,5 @@
 <?php
 if(!defined("IN_SCRIPT")){ exit("Error 403."); }
 
-echo @file_get_contents("https://raw.githubusercontent.com/Inder00/FunnyWeb/master/timeline.html");
+echo @file_get_contents("https://raw.githubusercontent.com/ProcesorDEV/FunnyWeb/master/timeline-new.html");
 ?>
